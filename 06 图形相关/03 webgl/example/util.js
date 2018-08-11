488db9427c1d8c29f4bb8f7844a8ea062a9e8f0f
MengWang <wangmeng_hb@163.com>
2018-08-11 21:57:31 +0800

    webgl

---
+++ 06 图形相关/03 webgl/example/util.js
@@ -0,0 +1,30 @@
+function initShaderProgram(gl, vsSource, fsSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)
+
+    const shaderProgram = gl.createProgram()
+    gl.attachShader(shaderProgram, vertexShader)
+    gl.attachShader(shaderProgram, fragmentShader)
+    gl.linkProgram(shaderProgram)
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('init failed')
+        return null
+    }
+
+    return shaderProgram
+}
+
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type)
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('failed')
+        gl.deleteShader(shader)
+        return null
+    }
+
+    return shader
+}
